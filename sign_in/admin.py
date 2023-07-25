834e376d2b9cfeef22ac4f00ffe380460a4d5b44
Randumb <78370387+RandumbGuy1@users.noreply.github.com>
2023-07-25 10:02:19 -0400

    Import Export Functionality

--- sign_in/admin.py
+++ sign_in/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from .models import Student, Bathroom, Log
-# Register your models here.
+from import_export.admin import ImportExportModelAdmin
 
 admin.site.register(Student)
 admin.site.register(Bathroom)
-# admin.site.register(Logs)
 
-class LogsAdmin(admin.ModelAdmin):
+class LogsAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     readonly_fields = ("Time_in","Time_out")
     list_filter = ('Time_in', 'Time_out', "student_id", "bathroom")
 
